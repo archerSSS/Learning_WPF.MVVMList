39edc3eead939babc959e8f1096dc01311a8d3a7
archyrysss <archyrysss@gmail.com>
2020-09-13 22:24:37 +0300

    WPF_1: MVVMLesson_v.0.2

--- WPFMVVMApplication/WPFMVVMApplication/View/MainWindow.xaml.cs
+++ WPFMVVMApplication/WPFMVVMApplication/View/MainWindow.xaml.cs
@@ -10,6 +10,7 @@ using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
@@ -23,9 +24,44 @@ namespace WPFMVVMApplication
     /// </summary>
     public partial class MainWindow : Window
     {
+        private bool listOpened;
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+
+        private void ShowList(object sender, RoutedEventArgs e)
+        {
+            double FromBottom = 0;
+            double ToBottom = 0;
+            if (listOpened)
+            {
+                TextBlockShow.Text = "Show List";
+                FromBottom = 300;
+            }
+            else
+            {
+                TextBlockShow.Text = "Hide List";
+                ToBottom = 300;
+            }
+            listOpened = !listOpened;
+
+            ThicknessAnimation ta = new ThicknessAnimation();
+            Storyboard sb= new Storyboard();
+            EasingFunctionBase efb = new PowerEase() { Power = 8 };
+            ta.From = new Thickness(0, 0, 0, FromBottom);
+            ta.To = new Thickness(0, 0, 0, ToBottom);
+            ta.Duration = new Duration(TimeSpan.FromSeconds(2));
+            ta.EasingFunction = efb;
+
+            sb.Children.Add(ta);
+            Storyboard.SetTargetProperty(ta, new PropertyPath(Grid.MarginProperty));
+            Storyboard.SetTargetName(ta, BorderBox.Name);
+            sb.Begin(this);
+            Storyboard.SetTargetName(ta, BorderBox.Name);
+            sb.Begin(this);
         }
     }
 }
